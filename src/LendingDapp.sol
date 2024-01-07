35dc83305f7d346fa963585cbb6de5914a7bde7b
OdionOseiwe <oseiweifebhor@gmail.com>
2024-01-07 22:11:32 +0100

    created MockToken

--- src/LendingDapp.sol
+++ src/LendingDapp.sol
@@ -1,22 +1,32 @@
 pragma solidity ^0.8.13;
 
-contract LendingDApp{
+import "./interface/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+
+contract LendingDApp is Ownable, ReentrancyGuard{
 
     /////////////////////////////// MAIN FUNCTIONS /////////////////////////////////////
 
-    function deposit(address Asset , uint256 amount) external   {
+    function deposit(address _Asset , uint256 _amount) external   {
 
     }
 
-    function borrow(uint256 amount) external{
+    function borrow(uint256 _amount) external{
 
     }
 
-    function repay(uint256 amount) external{
+    function repay(uint256 _amount) external{
 
     }
 
-    function liquidate(address asset ,address account, uint256 amount) external{
+    function liquidate(address _asset ,address _account, uint256 _amount) external{
+
+    }
+
+    function whitelistToken(address _token) external {
 
     }
 
