2160c8696b18bb56ac43110dfcbb815115e27e4b
OdionOseiwe <oseiweifebhor@gmail.com>
2024-01-07 21:50:52 +0100

    created main functions

--- src/LendingDapp.sol
+++ src/LendingDapp.sol
@@ -1,5 +1,24 @@
 pragma solidity ^0.8.13;
 
 contract LendingDApp{
-    
+
+    /////////////////////////////// MAIN FUNCTIONS /////////////////////////////////////
+
+    function deposit(address Asset , uint256 amount) external   {
+
+    }
+
+    function borrow(uint256 amount) external{
+
+    }
+
+    function repay(uint256 amount) external{
+
+    }
+
+    function liquidate(address asset ,address account, uint256 amount) external{
+
+    }
+
+    ///////////////////////////////////// MODIFIERS /////////////////////////////////
 }
